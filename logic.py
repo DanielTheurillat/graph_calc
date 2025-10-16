7dd327fdc0358822202294b75ac688dcfd975ec3
dani <daniel.theurillat05@gmail.com>
2025-10-16 22:52:16 +0200

    separato i pulsanti di base in Frame, implementati due dizionari per riconoscere gli input, aggiunto metodo look_dict, get_sol, implementata funzione di callback, aggiornata update e insert

--- logic.py
+++ logic.py
@@ -11,46 +11,77 @@ class Logic():
         self.flush = False
         self.sol = None
         self.default_dict = {
-            '0' : 0,
-            '1' : 1,
-            '2' : 2,
-            '3' : 3,
-            '4' : 4,
-            '5' : 5,
-            '6' : 6,
-            '7' : 7,
-            '8' : 8,
-            '9' : 9,
+            '0' : '0',
+            '1' : '1',
+            '2' : '2',
+            '3' : '3',
+            '4' : '4',
+            '5' : '5',
+            '6' : '6',
+            '7' : '7',
+            '8' : '8',
+            '9' : '9',
             '.' : '.',
             '+' : '+',
             '-' : '-',
             '\u00d7' : '*',
-            '/' : '/'
+            '/' : '/',
+            '*' : '*',
+            's' : 'sin',
+            '(' : '(',
+            ')' : ')',
         }
 
-    def update_stream(self,chars,*args):
-        self.stream.set(self.stream.get()+str(chars))
+        self.func_dict = {
+            '\r' : self.evaluate,
+            'Ans' : self.get_sol,
+        }
 
-    def insert(self,TOK):
+    def get_sol(self):
+        print(self.sol)
+        self.update(self.sol)
+
+    def look_dict(self,dict:dict,key):
+        if dict.get(key) is not None:
+            return dict[key]
+        else: 
+            return None
+
+    def insert(self,chars,*args):
         if self.flush: self.stream.set('');self.TOK_list=[];self.flush=False
-        if self.mode=='D':
-            try:
-                self.TOK_list.append(self.default_dict[str(TOK)])
-                self.update_stream(TOK)
-            except KeyError:
-                pass
+        if chars is not None:
+            self.TOK_list.append(chars)
+            self.stream.set(self.stream.get()+str(chars))
 
-    def callback(self,*args):
-        pass
+    def update(self,TOK):
+        TOK = str(TOK)
+        if self.mode=='D':
+            self.insert(self.look_dict(self.default_dict,TOK))
+            # if self.default_dict.get(TOK) is not None:
+            #     self.insert(self.default_dict[TOK])
+
+    def callback(self,event:tk.Event|str):
+        if type(event) == tk.Event:
+            print(f'Pulsante: {repr(event.char)}')
+            val = self.look_dict(self.func_dict,event.char)
+            if val is not None:
+                val()
+                return
+            self.update(event.char)
+        elif type(event) == str:
+            val = self.look_dict(self.func_dict,event)
+            if val is not None:
+                val()
+                return
+            self.update(event)
+        else: print('Errore di callback')
 
     def evaluate(self):
         try:
             self.sol = eval(''.join(str(x) for x in self.TOK_list))
             self.stream.set(self.sol)
+            self.TOK_list=[self.sol]
         except (SyntaxError, NameError, TypeError, ZeroDivisionError):
             self.stream.set('Errore')
             self.sol = None
-            pass
-        self.flush = True
-
-        
+            self.flush = True
